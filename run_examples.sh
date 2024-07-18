4c579fc798e27c210b1d5068760f72c6e935e2d6
Mikhail Tavarez <miktavarez@gmail.com>
2024-07-18 12:03:01 -0500

    update examples

--- run_examples.sh
+++ run_examples.sh
@@ -4,13 +4,11 @@ mojo package mist -I ./external -o ./temp/mist.mojopkg
 
 echo -e "Building binaries for all examples...\n"
 mojo build examples/hello_world/hello_world.mojo -o temp/hello_world
-mojo build tests/test_renderers.mojo -o temp/renderers
-mojo build tests/test_profiles.mojo -o temp/profiles
+mojo build examples/profiles.mojo -o temp/profiles
 
 echo -e "Executing examples...\n"
 cd temp
 ./hello_world
-./renderers
 ./profiles
 
 cd ..
