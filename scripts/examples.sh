00817fe6a22ba520b448e4927963d6c5b5aab4a6
Mikhail Tavarez <miktavarez@gmail.com>
2024-09-13 17:07:53 -0500

    add changelog

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -5,14 +5,14 @@ PACKAGE_NAME=mist
 mkdir -p $TEMP_DIR
 
 echo "[INFO] Building $PACKAGE_NAME package and example binaries."
-cp -R test/ $TEMP_DIR
+cp -R examples/ $TEMP_DIR
 magic run mojo package src/$PACKAGE_NAME -o $TEMP_DIR/$PACKAGE_NAME.mojopkg
-magic run mojo build examples/hello_world.mojo -o $TEMP_DIR/hello_world
-magic run mojo build examples/profiles.mojo -o $TEMP_DIR/profiles
+magic run mojo build $TEMP_DIR/hello_world.mojo -o $TEMP_DIR/hello_world
+magic run mojo build $TEMP_DIR/profiles.mojo -o $TEMP_DIR/profiles
 
 echo "[INFO] Running examples..."
-./$TEMP_DIR/hello_world
-./$TEMP_DIR/profiles
+$TEMP_DIR/hello_world
+$TEMP_DIR/profiles
 
 echo "[INFO] Cleaning up the example directory."
 rm -R $TEMP_DIR
